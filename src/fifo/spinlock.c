f97c32c31e76b309d907e64441f083528c68b658
Gwen Nelson <gwen@gwennelson.co.uk>
2026-08-30 20:03:35 +0100

    lock init and trylock

--- src/fifo/spinlock.c
+++ src/fifo/spinlock.c
@@ -3,14 +3,23 @@
  *
  * Copyright (C) 2026 Gwen Nelson
  *
- * GPLv2
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
+
+#include <stdatomic.h>
 
 #include <libfifo/sync.h>
 
-void fifo_spinlock_init(fifo_spinlock_t *lock)
-{
-    (void)lock;
+void fifo_spinlock_init(fifo_spinlock_t *lock) {
+     atomic_flag_clear(&lock->locked);
 }
 
 void fifo_spinlock_lock(fifo_spinlock_t *lock)
@@ -18,10 +27,8 @@ void fifo_spinlock_lock(fifo_spinlock_t *lock)
     (void)lock;
 }
 
-bool fifo_spinlock_trylock(fifo_spinlock_t *lock)
-{
-    (void)lock;
-    return false;
+bool fifo_spinlock_trylock(fifo_spinlock_t *lock) {
+     return !atomic_flag_test_and_set(&lock->locked);
 }
 
 void fifo_spinlock_unlock(fifo_spinlock_t *lock)
