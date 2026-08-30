eedd4ccef415b3759f4a9cc66a7c25b8232d097b
Gwen Nelson <gwen@gwennelson.co.uk>
2026-08-30 21:45:05 +0100

    semaphores

--- src/fifo/semaphore.c
+++ src/fifo/semaphore.c
@@ -3,30 +3,65 @@
  *
  * Copyright (C) 2026 Gwen Nelson
  *
- * GPLv2
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
+#include <stdatomic.h>
 #include <libfifo/sync.h>
 
-void fifo_semaphore_init(fifo_semaphore_t *semaphore,
-                         size_t initial_count)
-{
-    (void)semaphore;
-    (void)initial_count;
+void fifo_semaphore_init(fifo_semaphore_t *semaphore, size_t initial_count){
+     atomic_store_explicit(&semaphore->count, initial_count, memory_order_relaxed);
 }
 
-void fifo_semaphore_wait(fifo_semaphore_t *semaphore)
-{
-    (void)semaphore;
+void fifo_semaphore_wait(fifo_semaphore_t *semaphore) {
+     size_t count;
+
+     for (;;) {
+         count = atomic_load_explicit(&semaphore->count,
+                                     memory_order_relaxed);
+
+         while (count != 0) {
+            if (atomic_compare_exchange_weak_explicit(
+                    &semaphore->count,
+                    &count,
+                    count - 1,
+                    memory_order_acquire,
+                    memory_order_relaxed))
+                return;
+         }
+
+         fifo_platform_yield();
+     }
 }
 
-bool fifo_semaphore_trywait(fifo_semaphore_t *semaphore)
-{
-    (void)semaphore;
-    return false;
+bool fifo_semaphore_trywait(fifo_semaphore_t *semaphore) {
+     size_t count;
+
+     count = atomic_load_explicit(&semaphore->count,
+                                 memory_order_relaxed);
+
+     while (count != 0) {
+        if (atomic_compare_exchange_weak_explicit(
+                &semaphore->count,
+                &count,
+                count - 1,
+                memory_order_acquire,
+                memory_order_relaxed))
+            return true;
+     }
+
+     return false;
 }
 
-void fifo_semaphore_post(fifo_semaphore_t *semaphore)
-{
-    (void)semaphore;
+void fifo_semaphore_post(fifo_semaphore_t *semaphore) {
+     atomic_fetch_add_explicit(&semaphore->count, 1, memory_order_release);
 }
+
