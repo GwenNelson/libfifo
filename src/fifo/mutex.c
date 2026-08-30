edc7fec18da893474c8d0424eb531247a2c51f8b
Gwen Nelson <gwen@gwennelson.co.uk>
2026-08-30 21:37:25 +0100

    mutex implementation

--- src/fifo/mutex.c
+++ src/fifo/mutex.c
@@ -3,28 +3,48 @@
  *
  * Copyright (C) 2026 Gwen Nelson
  *
- * GPLv2
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
+#include <stdatomic.h>
 #include <libfifo/sync.h>
 
-void fifo_mutex_init(fifo_mutex_t *mutex)
-{
-    (void)mutex;
+void fifo_mutex_init(fifo_mutex_t *mutex) {
+    atomic_store_explicit(&mutex->state, 0, memory_order_relaxed);
 }
 
-void fifo_mutex_lock(fifo_mutex_t *mutex)
-{
-    (void)mutex;
+void fifo_mutex_lock(fifo_mutex_t *mutex) {
+     unsigned int expected;
+
+     for (;;) {
+         expected = 0;
+
+         if (atomic_compare_exchange_strong_explicit(&mutex->state, &expected, 1, memory_order_acquire, memory_order_relaxed)) {
+	      return; 
+	 } else {
+              fifo_platform_yield();
+	 }
+     }
 }
 
-bool fifo_mutex_trylock(fifo_mutex_t *mutex)
-{
-    (void)mutex;
-    return false;
+bool fifo_mutex_trylock(fifo_mutex_t *mutex) {
+     unsigned int expected = 0;
+
+     return atomic_compare_exchange_strong_explicit(&mutex->state,
+                                                    &expected,
+                                                    1,
+                                                    memory_order_acquire,
+                                                    memory_order_relaxed);
 }
 
-void fifo_mutex_unlock(fifo_mutex_t *mutex)
-{
-    (void)mutex;
+void fifo_mutex_unlock(fifo_mutex_t *mutex) {
+     atomic_store_explicit(&mutex->state, 0, memory_order_release);
 }
